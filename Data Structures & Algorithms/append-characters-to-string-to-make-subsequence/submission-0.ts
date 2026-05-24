7eb73aaf83e8d3dba18c502945cf18d5c6474d68
Odukwe Daniel <odukwedaniel1@gmail.com>
2026-05-24 23:56:04 +0100

    Add: append-characters-to-string-to-make-subsequence - submission-0

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-0.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {number}
+     */
+    appendCharacters(s: string, t: string): number {
+        let i = 0;
+        let j = 0;
+        while (i < s.length) {
+            if (t[j] === s[i]) {
+                j++;
+            }
+            i++;
+        }
+
+        return t.length - j;
+    }
+}
